cc7c3a24eea68b433a997a2d7590bcf457d007bd
uaesmartlink <team.uaesmartlink@gmail.com>
2023-02-24 10:26:53 +0400

    init state for demo

--- resources/views/backend/clients/create.blade.php
+++ resources/views/backend/clients/create.blade.php
@@ -521,12 +521,12 @@
         console.log("Hello");
         var id = 231;
         $.get("{{route('admin.shipments.get-states-ajax')}}?country_id=" + id, function(data) {
-            $('select[name ="address[0][\"state_id\"]"]').empty();
-            $('select[name ="address[0][\"state_id\"]"]').append('<option value=""></option>');
+            $('select[name ="address[0][state_id]"]').empty();
+            $('select[name ="address[0][state_id]"]').append('<option value=""></option>');
             for (let index = 0; index < data.length; index++) {
                 const element = data[index];
                 console.log(element['name']);
-                $('select[name ="address[0][\"state_id\"]"]').append('<option value="' + element['id'] + '">' + element['name'] + '</option>');
+                $('select[name ="address[0][state_id]"]').append('<option value="' + element['id'] + '">' + element['name'] + '</option>');
             }
         });
     }
