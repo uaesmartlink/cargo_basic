c58fb4197a3daf6c455afcd002aa462248df9290
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-03 10:11:52 +0400

    codeId

--- resources/views/backend/shipments/create.blade.php
+++ resources/views/backend/shipments/create.blade.php
@@ -224,8 +224,8 @@
                                         <input type="text" placeholder="" class="form-control" name="client-id" value="{{$auth_user->name}}"  >
                                         <input type="hidden" name="Shipment[client_id]" value="{{$auth_user->userClient->id}}">
                                     @else
-                                    <input type="hidden" placeholder="" class="form-control" name="client_id" id="client_id" >
-                                        <select class="form-control kt-select2 select-client" id="client-id" onchange="selectIsTriggered()" name="Shipment[client_id]"  disabled>
+                                    <input type="hidden" placeholder="" class="form-control" name="Shipment[client_id]" id="client_id" >
+                                        <select class="form-control kt-select2 select-client" id="client-id" onchange="selectIsTriggered()" name="client_id"  disabled>
                                             <option></option>
                                             @foreach($clients as $client)
                                                     <option value="{{$client->id}}" data-phone="{{$client->responsible_mobile}}">{{$client->name}}</option>
