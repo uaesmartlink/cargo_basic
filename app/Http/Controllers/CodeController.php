b93b70491265bb38f7c7754b532a5133d1597619
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-03 19:32:39 +0400

    show order

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -33,9 +33,7 @@ class CodeController extends Controller
         else{
             $codeId = $code->id + 1;
         }
-        $histories = HistoryCodes::orderBy('id','desc');
-        $histories = $histories->with('clients');
-        dd($histories);
+        $histories = $histories->with('client')->orderBy('id','desc');
         return view('backend.codes.create', compact('clients','codeId','histories'));
     }
 
@@ -74,8 +72,7 @@ class CodeController extends Controller
             $history->qty = $qty;
             $history->save();
             $clients = Client::all();
-            $histories = HistoryCodes::orderBy('id','desc');
-            $histories = $histories->with('client');
+            $histories = $histories->with('client')->orderBy('id','desc');
             return view('backend.codes.create', compact('clients','codeId','histories'));
 
         }catch (\Exception $e) {
