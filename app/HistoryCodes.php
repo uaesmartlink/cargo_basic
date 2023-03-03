21d6d9b86b341f3b4506385083009b5b2c56a0bc
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-03 19:41:54 +0400

    show order

--- app/HistoryCodes.php
+++ app/HistoryCodes.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HistoryCodes extends Model
 {
+    protected $table = 'history_codes';
     public function client(): BelongsTo
     {
         return $this->belongsTo(Client::class,'client_id');
