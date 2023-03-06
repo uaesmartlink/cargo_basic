cdff2dfc164bf256faf5526546fa4ecd90b84a24
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-06 23:11:42 +0400

    missions report

--- resources/views/backend/missions/missions-report.blade.php
+++ resources/views/backend/missions/missions-report.blade.php
@@ -28,7 +28,7 @@
 
     <div class="card-body">
         <!--begin::Search Form-->
-        <form method="POST" action="{{route('admin.missions.post.get-report')}}" >
+        <form method="POST" action="{{route('admin.missions.post.get.report')}}" >
             @csrf
             <div class="mb-7">
                 <div class="row align-items-center">
