9e031ff17ff6a3cbeed7cec89375ed1cadb42a68
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-07 11:07:35 +0400

    new 'mission report'

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -56,6 +56,7 @@ class ShipmentController extends Controller
         $type = null;
         $sort_by = null;
         if (isset($_GET)) {
+            dd($_GET);
             if (isset($_GET['code']) && !empty($_GET['code'])) {
 
                 $shipments = $shipments->where('code', $_GET['code']);
