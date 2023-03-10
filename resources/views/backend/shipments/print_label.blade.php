a7c0eee8e984346d5ca1139564c768fb68447e34
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-10 10:59:57 +0400

    print label and sticker hide second site

--- resources/views/backend/shipments/print_label.blade.php
+++ resources/views/backend/shipments/print_label.blade.php
@@ -32,7 +32,7 @@ $d = new DNS1D();
                                             @if(get_setting('system_logo_white') != null)
                                                 <img src="{{ uploaded_asset(get_setting('system_logo_white')) }}">
                                             @else
-                                                <img src="{{ static_asset('assets/img/logo.svg') }} " style="width: 50px">
+                                                <img src="{{ static_asset('assets/img/logo.svg') }} " width="50px" height="50px">
                                             @endif
                                         </td>
                                     </tr>
