0b93d4227366c33e8aced9fec2a8ff32b3cec6dc
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-10 15:04:22 +0400

    edit client

--- resources/views/backend/clients/index.blade.php
+++ resources/views/backend/clients/index.blade.php
@@ -19,7 +19,47 @@
     <div class="card-header">
         <h5 class="mb-0 h6">{{translate('Customers')}}</h5>
     </div>
+    <div class="card-body">
+        <table class="table aiz-table mb-0">
+            <thead>
+                <tr>
+                    <th  width="3%">#</th>
+                    <th >{{translate('Name')}}</th>
+                    <th >{{translate('Email')}}</th>
+                    <th >{{translate('Phone')}}</th>
 
+                    <th  width="10%" class="text-center">{{translate('Options')}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($clients as $key => $client)
+
+                        <tr>
+                            <td  width="3%">{{ ($key+1) + ($clients->currentPage() - 1)*$clients->perPage() }}</td>
+                            <td width="20%">{{$client->name}}</td>
+                            <td width="20%">{{$client->email}}</td>
+                            <td width="20%">{{$client->responsible_mobile}}</td>
+
+                            <td class="text-center">
+                                    <a class="btn btn-soft-primary btn-icon btn-circle btn-sm" href="{{route('admin.clients.show', $client->id)}}" title="{{ translate('Show') }}">
+		                                <i class="las la-eye"></i>
+		                            </a>
+		                            <a class="btn btn-soft-primary btn-icon btn-circle btn-sm" href="{{route('admin.clients.edit', $client->id)}}" title="{{ translate('Edit') }}">
+		                                <i class="las la-edit"></i>
+		                            </a>
+		                            <a href="#" class="btn btn-soft-danger btn-icon btn-circle btn-sm confirm-delete" data-href="{{route('admin.clients.delete-client', ['client'=>$client->id])}}" title="{{ translate('Delete') }}">
+		                                <i class="las la-trash"></i>
+		                            </a>
+		                        </td>
+                        </tr>
+
+                @endforeach
+            </tbody>
+        </table>
+        <div class="aiz-pagination">
+            {{ $clients->appends(request()->input())->links() }}
+        </div>
+    </div>
 </div>
 {!! hookView('spot-cargo-shipment-client-addon',$currentView) !!}
 
