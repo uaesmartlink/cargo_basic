55b2a8f427fe779fb04efb2c184666e5898e7ceb
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-10 10:59:11 +0400

    print label and sticker hide second site

--- resources/views/backend/shipments/print_label.blade.php
+++ resources/views/backend/shipments/print_label.blade.php
@@ -22,7 +22,7 @@ $d = new DNS1D();
         <table border="0" cellpadding="0" cellspacing="0" style="font-size:10px;font-family:Arial, Helvetica, sans-serif; ">
             <tr>
                 <td>
-                    <table width="200" border="0" cellpadding="0" cellspacing="0" style="font-size:16px;font-family:Arial, Helvetica, sans-serif;">
+                    <table width="450px" border="0" cellpadding="0" cellspacing="0" style="font-size:16px;font-family:Arial, Helvetica, sans-serif;">
                         <tr>
                             <td height="21px" colspan="3" style="padding: auto;">
                                 <table style="padding: auto;" border="0" align="center" cellpadding="0" cellspacing="0">
@@ -32,7 +32,7 @@ $d = new DNS1D();
                                             @if(get_setting('system_logo_white') != null)
                                                 <img src="{{ uploaded_asset(get_setting('system_logo_white')) }}">
                                             @else
-                                                <img src="{{ static_asset('assets/img/logo.svg') }} " style="widows: 40%">
+                                                <img src="{{ static_asset('assets/img/logo.svg') }} " style="width: 50px">
                                             @endif
                                         </td>
                                     </tr>
